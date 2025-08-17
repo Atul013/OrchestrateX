5cbd2fd7bbf7b692283430529793303eaaff74de
Jinendran10 <jinendranjinu10@gmail.com>
2025-08-17 22:35:17 +0530

    rewind

---
+++ netflix/netflix-clone/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Netflix Clone</h1>
+            <section>
+                <h2>Featured Content</h2>
+                {/* Add featured content here */}
+            </section>
+            <section>
+                <h2>Popular Movies</h2>
+                {/* Add list of popular movies here */}
+            </section>
+            <section>
+                <h2>Trending Shows</h2>
+                {/* Add list of trending shows here */}
+            </section>
+        </div>
+    );
+};
+
+export default Home;
